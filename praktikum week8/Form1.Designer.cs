a39cff8241af2549ff2dd437e82847e60268cf43
jocelynleora <95455125+jocelynleora@users.noreply.github.com>
2022-04-11 11:04:37 +0700

    Add files via upload

---
+++ praktikum week8/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace praktikum_week8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbox_1 = new System.Windows.Forms.ComboBox();
+            this.cbox_2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbl_manager1 = new System.Windows.Forms.Label();
+            this.lbl_manager2 = new System.Windows.Forms.Label();
+            this.lbl_captain1 = new System.Windows.Forms.Label();
+            this.lbl_captain2 = new System.Windows.Forms.Label();
+            this.lbl_namaStadium = new System.Windows.Forms.Label();
+            this.lbl_capacity = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbox_1
+            // 
+            this.cbox_1.FormattingEnabled = true;
+            this.cbox_1.Location = new System.Drawing.Point(71, 65);
+            this.cbox_1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbox_1.Name = "cbox_1";
+            this.cbox_1.Size = new System.Drawing.Size(252, 24);
+            this.cbox_1.TabIndex = 0;
+            this.cbox_1.SelectedIndexChanged += new System.EventHandler(this.cbox_1_SelectedIndexChanged);
+            // 
+            // cbox_2
+            // 
+            this.cbox_2.FormattingEnabled = true;
+            this.cbox_2.Location = new System.Drawing.Point(524, 65);
+            this.cbox_2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbox_2.Name = "cbox_2";
+            this.cbox_2.Size = new System.Drawing.Size(252, 24);
+            this.cbox_2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(399, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "VS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 135);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Manager:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(520, 135);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Manager:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 190);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Captain:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(520, 190);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Captain:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(263, 295);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Stadium:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(265, 347);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Capacity:";
+            // 
+            // lbl_manager1
+            // 
+            this.lbl_manager1.AutoSize = true;
+            this.lbl_manager1.Location = new System.Drawing.Point(144, 135);
+            this.lbl_manager1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_manager1.Name = "lbl_manager1";
+            this.lbl_manager1.Size = new System.Drawing.Size(68, 16);
+            this.lbl_manager1.TabIndex = 9;
+            this.lbl_manager1.Text = "manager1";
+            // 
+            // lbl_manager2
+            // 
+            this.lbl_manager2.AutoSize = true;
+            this.lbl_manager2.Location = new System.Drawing.Point(597, 135);
+            this.lbl_manager2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_manager2.Name = "lbl_manager2";
+            this.lbl_manager2.Size = new System.Drawing.Size(68, 16);
+            this.lbl_manager2.TabIndex = 10;
+            this.lbl_manager2.Text = "manager2";
+            // 
+            // lbl_captain1
+            // 
+            this.lbl_captain1.AutoSize = true;
+            this.lbl_captain1.Location = new System.Drawing.Point(144, 190);
+            this.lbl_captain1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_captain1.Name = "lbl_captain1";
+            this.lbl_captain1.Size = new System.Drawing.Size(58, 16);
+            this.lbl_captain1.TabIndex = 11;
+            this.lbl_captain1.Text = "captain1";
+            // 
+            // lbl_captain2
+            // 
+            this.lbl_captain2.AutoSize = true;
+            this.lbl_captain2.Location = new System.Drawing.Point(597, 190);
+            this.lbl_captain2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_captain2.Name = "lbl_captain2";
+            this.lbl_captain2.Size = new System.Drawing.Size(58, 16);
+            this.lbl_captain2.TabIndex = 12;
+            this.lbl_captain2.Text = "captain2";
+            // 
+            // lbl_namaStadium
+            // 
+            this.lbl_namaStadium.AutoSize = true;
+            this.lbl_namaStadium.Location = new System.Drawing.Point(349, 295);
+            this.lbl_namaStadium.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_namaStadium.Name = "lbl_namaStadium";
+            this.lbl_namaStadium.Size = new System.Drawing.Size(96, 16);
+            this.lbl_namaStadium.TabIndex = 13;
+            this.lbl_namaStadium.Text = "Nama Stadium";
+            // 
+            // lbl_capacity
+            // 
+            this.lbl_capacity.AutoSize = true;
+            this.lbl_capacity.Location = new System.Drawing.Point(349, 347);
+            this.lbl_capacity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_capacity.Name = "lbl_capacity";
+            this.lbl_capacity.Size = new System.Drawing.Size(119, 16);
+            this.lbl_capacity.TabIndex = 14;
+            this.lbl_capacity.Text = "Kapasitas Stadium";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lbl_capacity);
+            this.Controls.Add(this.lbl_namaStadium);
+            this.Controls.Add(this.lbl_captain2);
+            this.Controls.Add(this.lbl_captain1);
+            this.Controls.Add(this.lbl_manager2);
+            this.Controls.Add(this.lbl_manager1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbox_2);
+            this.Controls.Add(this.cbox_1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbox_1;
+        private System.Windows.Forms.ComboBox cbox_2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_manager1;
+        private System.Windows.Forms.Label lbl_manager2;
+        private System.Windows.Forms.Label lbl_captain1;
+        private System.Windows.Forms.Label lbl_captain2;
+        private System.Windows.Forms.Label lbl_namaStadium;
+        private System.Windows.Forms.Label lbl_capacity;
+    }
+}
+
